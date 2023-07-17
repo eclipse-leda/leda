3509b82e2b2d19a5101e2f470bcc803c7be92c05
Vasil Ivanov <vasil.ivanov3@bosch.io>
2023-07-17 07:46:25 +0000

    Working consent form for GA

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,7 +1,24 @@
 import * as params from '@params';
 
-analyticsID = params["analyticsID"]
 
+analyticsID = params["googleAnalyticsID"];
 
-console.log(params["analyticsID"])
+// If the GA ID was not defined in the hugo config, it would be set as null
+// Therefore we should skip execution in those cases
+if (analyticsID !== null) {
+    console.log(`GA enabled for ID ${analyticsID}`);
 
+    const gaScript = document.createElement("script");
+    gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${analyticsID}`;
+    gaScript.async = true;
+    document.head.appendChild(gaScript);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', analyticsID);
+} else {
+    console.log("Google Analytics enabled, but ID was never defined.")
+}
